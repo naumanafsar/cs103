b1b78bf2a0345a15256250ce7c0378c52a0ba738
Mr <p176053@nu.edu.pk>
2018-04-19 01:16:11 +0500

    Reading File!

---
+++ File Handling/reading-file.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main () {
+  string inFileName = "test.txt";
+
+  fstream inFile;
+
+  inFile.open("text.txt", ios::in);
+
+  if(inFile.is_open()) {
+    string line;
+
+    while(!inFile.eof()) {      //To read the entire file, eof suggests End of Line
+      getline(inFile, line);
+
+      // inFile >> line;
+
+      cout  << line << endl;
+    }
+
+
+
+    inFile.close();
+  }
+
+  else {
+    cout << "Could not open the file ";
+  }
+}
